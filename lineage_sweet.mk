b35b5a763f41a5f8d26eae89805ba36cb0ebe9e7
Rajeevan <81076257+Raj162002@users.noreply.github.com>
2022-02-26 07:33:38 +0530

    Bring up 1
    Changes in make file 
    Still have to change the path of inherit

--- lineage_sweet.mk
+++ lineage_sweet.mk
@@ -8,7 +8,7 @@
 $(call inherit-product, device/xiaomi/sweet/device.mk)
 
 # Inherit some common SuperiorOS stuff.
-$(call inherit-product, vendor/superior/config/common.mk)
+$(call inherit-product, vendor/lineage/config/common.mk)
 
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_INCLUDE_PIXEL_CHARGER := true
@@ -17,7 +17,7 @@ TARGET_SUPPORTS_QUICK_TAP := true
 TARGET_USES_FACE_UNLOCK := true
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := superior_sweet
+PRODUCT_NAME := lineage_sweet
 PRODUCT_DEVICE := sweet
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi Note 10 Pro
@@ -25,5 +25,4 @@ PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
-# SuperiorOS
-SUPERIOR_OFFICIAL := true
+
